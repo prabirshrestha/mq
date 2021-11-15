3b5ff29e86c5fa0833528ce8e9bb3f72dba5959f
Prabir Shrestha <mail@prabir.me>
2021-11-14 19:46:27 -0800

    add consume()

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub use async_trait::async_trait;
 
 #[async_trait]
 pub trait MessageQueue {
-    async fn create_queue(queue_name: &str);
-    async fn delete_queue(queue_name: &str);
+    async fn create_queue(&mut self, queue_name: &str);
+    async fn delete_queue(&mut self, queue_name: &str);
+
+    async fn consume(&mut self, queue_name: &str);
 }
