e53a0d1fb383a4f5d259d5cb0da6102b9dd40d4d
Prabir Shrestha <mail@prabir.me>
2021-11-14 19:50:22 -0800

    rename to enqueue and dequeue

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ pub trait MessageQueue {
     async fn create_queue(&mut self, queue_name: &str);
     async fn delete_queue(&mut self, queue_name: &str);
 
-    async fn consume(&mut self, queue_name: &str);
-    async fn send(&mut self, queue_name: &str);
+    async fn dequeue(&mut self, queue_name: &str);
+    async fn enqueue(&mut self, queue_name: &str);
 
     async fn ack(&mut self, message_id: &str);
     async fn nack(&mut self, message_id: &str);
