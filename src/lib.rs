70971b2b73ab998d11c694daec3cba3c12a29efd
Prabir Shrestha <mail@prabir.me>
2022-06-05 14:13:50 -0700

    add MqMessage, MqRequest, MqError, BlackholeMessageBroker

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,43 @@ pub mod broker;
 
 pub use async_trait::async_trait;
 
+#[derive(Debug)]
+pub struct MqMessageBytes(Vec<u8>);
+
+#[derive(Debug)]
+pub struct MqMessage<T: TryFrom<MqMessageBytes>> {
+    pub id: String,
+    pub data: T,
+}
+
 #[async_trait]
 pub trait MessageQueue {
-    async fn create_queue(&mut self, queue_name: &str);
-    async fn delete_queue(&mut self, queue_name: &str);
+    async fn create_queue(&mut self, queue_name: &str) -> MqResult<()>;
+    async fn delete_queue(&mut self, queue_name: &str) -> MqResult<()>;
 
-    async fn dequeue(&mut self, queue_name: &str);
-    async fn enqueue(&mut self, queue_name: &str);
+    async fn dequeue<E: TryFrom<MqMessageBytes, Error = Vec<u8>>>(
+        &mut self,
+        queue_name: &str,
+        visiblity_timeout_in_ms: Option<u64>,
+    ) -> MqResult<Option<MqMessage<E>>>;
 
-    async fn ack(&mut self, message_id: &str);
-    async fn nack(&mut self, message_id: &str);
+    async fn enqueue<M: Into<MqMessageBytes> + Send>(
+        &mut self,
+        queue_name: &str,
+        message: M,
+    ) -> MqResult<String>;
 
-    async fn ping(&mut self);
+    async fn ack(&mut self, message_id: &str) -> MqResult<()>;
+
+    async fn nack(&mut self, message_id: &str) -> MqResult<()>;
+
+    async fn ping(&mut self) -> MqResult<()>;
 }
+
+#[derive(thiserror::Error, Debug)]
+pub enum MqError {
+    #[error("Cannot decode message.")]
+    CannotDecodeMessage(Vec<u8>),
+}
+
+pub type MqResult<T> = Result<T, MqError>;
